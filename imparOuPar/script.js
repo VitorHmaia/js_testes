e8ac20b01a4027d1a01ef371546e9214492a8237
VitorHmaia <vitorhmaia@outlook.com>
2023-02-09 14:27:59 -0300

    final

--- imparOuPar/script.js
+++ imparOuPar/script.js
@@ -1,4 +1,4 @@
 //Declara var número
 var n = prompt('Digite um número');
-var resultado = n % 2 ===0? 'par':'impar'; //Compara o resto do resulto e verifica se é estritamente igual 
+var resultado = n % 2 ===0? 'par':'impar'; //Compara o resto do resulto e verifica se é estritamente igual - função ternária ( ?: ), logo, compara uma condição true or false, e retorna uma expressão caso seja true e outra caso seja false
     alert(`número ${n} é ${resultado}`); // mostra var e se ele é par ou ímpar, caso a entradar var for string, sempre dará ímpar
