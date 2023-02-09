b64f8dab0823d9c53e7d056c45231af26aa62416
VitorHmaia <vitorhmaia@outlook.com>
2023-02-09 14:14:37 -0300

    par ou impar added

---
+++ imparOuPar/script.js
@@ -0,0 +1,4 @@
+//Declara var número
+var n = prompt('Digite um número');
+var resultado = n % 2 ===0? 'par':'impar'; //Compara o resto do resulto e verifica se é estritamente igual 
+    alert(`número ${n} é ${resultado}`); // mostra var e se ele é par ou ímpar, caso a entradar var for string, sempre dará ímpar
