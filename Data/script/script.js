c632c2b336f0eb53c9e3a287c781965d8f2a99d1
VitorHmaia <vitorhmaia@outlook.com>
2023-02-10 14:20:53 -0300

    desafios da data

---
+++ Data/script/script.js
@@ -0,0 +1,62 @@
+
+var data = new Date()
+var hora = data.getHours()
+var dia = data.getDay()
+var min = data.getMinutes()
+
+
+/*let data = new Date();
+function formatarData(data){ //formatar data em xx/xx/xxx
+    let newDate = new Date(data1);
+    return `${newDate.getDate()/newDate.getMonth()+1/newDate.getFullYear()}`;
+}
+console.log(formatarData(data));
+*/
+
+
+//Desafio 1 - Bom dia, Tarde e Noite
+    if(hora<12){
+        console.log(`Bom dia! São ${hora} horas e ${min} minutos`)
+        }
+        else if(hora>18 && hora<24){
+            console.log(`Boa Noite! São ${hora} horas e ${min} minutos`)
+        }
+            else{
+                console.log(`Boa Tarde! São ${hora} horas e ${min} minutos`)
+                }
+
+//Desafio 2
+console.log(dia)
+
+    switch(dia){
+        case 0:
+            console.log(`Hoje é Domingo`);
+            break;
+
+        case 1:
+            console.log(`Hoje é Segunda-feira`);
+            break;
+
+        case 2:
+            console.log(`Hoje é Terça-feira`);
+            break;
+
+        case 3:
+            console.log(`Hoje é Quarta-feira`);
+            break; 
+
+        case 4:
+            console.log(`Hoje é Quinta-feira`);
+            break; 
+
+        case 5:
+            console.log(`Hoje é Sexta-feira`);
+            break; 
+
+        default:
+            console.log(`Hoje é Sábado`);
+    }
+
+
+
+
